f41e51ebd6f532d8654ffe51c926207c8e499bf3
Saqib Ismail <saqibismail003@gmail.com>
2026-04-30 08:43:50 -0500

    diff done elo started

--- src/data/cleanCsv.ts
+++ src/data/cleanCsv.ts
@@ -1,31 +1,188 @@
 import fs from "node:fs";
 import { parse } from "csv-parse";
-import { headers, type MatchRow, type RawMatchRow } from "../types/match.js";
+import { headers, type MatchRow, type RawMatchRow, type GameOutcomeInfo, type MatchRowWithGoals, ELO_K_MAP } from "../types/match.js";
 
 const normalizeTournamentName = (tournament: string): string => {
-  const normalizedTournamentMap: Record<string, string> = {
-    "Copa América": "Copa America",
-    "Copa América qualifier": "Copa America qualifier",
-    "European Nations League": "UEFA Nations League",
-    "European Nations League A": "UEFA Nations League",
-    "European Nations League B": "UEFA Nations League",
-    "European Nations League C": "UEFA Nations League",
-    "European Nations League D": "UEFA Nations League",
-    "European Nations League A/B": "UEFA Nations League",
-    "European Nations League B/C": "UEFA Nations League",
-    "CONCACAF Nations League": "CONCACAF Nations League",
-    "CONCACAF Nations League A": "CONCACAF Nations League",
-    "CONCACAF Nations League B": "CONCACAF Nations League",
-    "CONCACAF Nations League C": "CONCACAF Nations League",
-    "CONCACAF Nations League q": "CONCACAF Nations League qualifier",
-    "Asian Cup & Asian Chlg Cup q": "Asian Cup qualification",
-    "Asian Cup q & Asian Chlg Cup": "Asian Cup qualification",
-    "Asian Cup qualifier": "Asian Cup qualification",
-    "Oceania Nations Cup qualifier": "Oceania Nations Cup qualification",
-  };
+  const worldCup = new Set<string>([
+    "World Cup",
+  ]);
 
-  return normalizedTournamentMap[tournament] ?? tournament;
+  const worldCupQualifier = new Set<string>([
+    "World Cup qualifier",
+    "World Cup and African Cup qual",
+    "World Cup and Asian Cup qual",
+    "World Cup and CONCACAF Ch q",
+    "World Cup q & British Ch",
+    "World Cup q & Nordic Ch",
+    "World Cup q and CONCACAF Ch",
+    "W Cup and Oce Cup q and S Pac G",
+    "WC and Oce Cup q",
+    "WC q and Oce Cup",
+    "NA Champ & WC qual",
+  ]);
+
+  const continentalChampionship = new Set<string>([
+    "African Nations Cup",
+    "Asian Cup",
+    "CONCACAF Championship",
+    "CCCF Championship",
+    "Copa America",
+    "Copa América",
+    "European Championship",
+    "North American Champ",
+    "Oceania Nations Cup",
+    "Panamerican Championship",
+    "South American Champ",
+    "Confederations Cup",
+  ]);
+
+  const continentalQualifier = new Set<string>([
+    "African Nations Cup qualifier",
+    "Asian Cup qualifier",
+    "Asian Cup & Asian Chlg Cup q",
+    "Asian Cup q & Asian Chlg Cup",
+    "CONCACAF Champ qual",
+    "Copa America qualifier",
+    "Copa América qualifier",
+    "European Championship qual",
+    "Oceania Nations Cup qualifier",
+    "South American Champ qual",
+    "Afr Games q & C Afr Cup",
+    "Afr Games q & W Afr Cup",
+  ]);
+
+  const nationsLeague = new Set<string>([
+    "European Nations League",
+    "European Nations League A",
+    "European Nations League B",
+    "European Nations League C",
+    "European Nations League D",
+    "European Nations League A/B",
+    "European Nations League B/C",
+    "European Nations League C/D",
+    "CONCACAF Nations League",
+    "CONCACAF Nations League A",
+    "CONCACAF Nations League B",
+    "CONCACAF Nations League C",
+    "CONCACAF Nations League q",
+  ]);
+
+  const regionalChampionship = new Set<string>([
+    "AFC-OFC Challenge Cup",
+    "African Games",
+    "Afro-Asian Cup",
+    "Afro-Asian Games",
+    "Amilcar Cabral Cup",
+    "Arab Cup",
+    "Asian Challenge Cup",
+    "Asian Games",
+    "Asian Solidarity Cup",
+    "Balkan Cup",
+    "Baltic Cup",
+    "Baltic Tournament",
+    "Bangabandhu Gold Cup",
+    "Bolivarian Games",
+    "British Championship",
+    "CECAFA Cup",
+    "CEDEAO Cup",
+    "CEMAC Cup",
+    "CFU Championship",
+    "COMESA Cup",
+    "CONCACAF Cup",
+    "CONCACAF Series",
+    "COSAFA Cup",
+    "Caribbean Championship",
+    "Caribbean Cup",
+    "Central African Games",
+    "Central American Cup",
+    "Central American Games",
+    "Central Asian Nations Cup",
+    "East Asian Championship",
+    "East Asian Games",
+    "Gulf Cup",
+    "Indian Ocean Cup",
+    "Indian Ocean Games",
+    "Indian Ocean Tournament",
+    "Islamic Games",
+    "Island Games",
+    "King's Cup",
+    "Korea Cup",
+    "Leeward Islands Champ",
+    "Mediterranean Cup",
+    "Mediterranean Games",
+    "Melanesian Cup",
+    "Micronesian Cup",
+    "Micronesian Games",
+    "N Am Ch & UNCAF Cup q",
+    "N Am Ch q & UNCAF Cup",
+    "Nehru Gold Cup",
+    "Olympic Games",
+    "Pacific Cup",
+    "Pacific Games",
+    "Pacific Mini Games",
+    "Pan Arab Games",
+    "Pan Arab Games & Arab Cup",
+    "Panamerican Games",
+    "SE Asian Peninsula Games",
+    "South Asian Championship",
+    "South Asian Games",
+    "South Asian Super Cup",
+    "South Pacific Games",
+    "South Pacific Mini Games",
+    "Southeast Asian Champ",
+    "Southeast Asian Games",
+    "Trans-Caucasian Champ",
+    "UNCAF Nations Cup",
+    "West African Cup",
+    "West African Nations Cup",
+    "West Asian Championship",
+    "West Asian Games",
+    "Windward Islands Champ",
+    "Zone V Tournament",
+  ]);
+
+  const regionalQualifier = new Set<string>([
+    "African Games qualifier",
+    "Arab Cup qualifier",
+    "Caribbean Championship qual",
+    "Caribbean Cup qualifier",
+    "CEDEAO Cup qualifier",
+    "CFU Championship qualifier",
+    "CONCACAF Ch & Car Ch q",
+    "CONCACAF Ch & Car Cup q",
+    "CONCACAF Ch q & C Am Cup",
+    "CONCACAF Ch q & Car Ch",
+    "CONCACAF Ch q & Car Ch PO",
+    "CONCACAF Ch q & Car Cup",
+    "East Asian Championship qual",
+    "Euro Ch q & British Ch",
+    "Euro Ch q & Nordic Ch",
+    "Oce Cup q & Melanesian Cup",
+    "Oce Cup q & Polynesian Cup",
+    "Oly qual & Nrd Ch",
+    "Olympic Games qualifier",
+    "Pan American Games qualifier",
+    "Southeast Asian Champ qual",
+  ]);
+
+  const friendly = new Set<string>([
+    "Friendly",
+    "Friendly tournament",
+    "FIFA Series",
+  ]);
+
+  if (worldCup.has(tournament)) return "world_cup";
+  if (worldCupQualifier.has(tournament)) return "world_cup_qualifier";
+  if (continentalChampionship.has(tournament)) return "continental_championship";
+  if (continentalQualifier.has(tournament)) return "continental_qualifier";
+  if (nationsLeague.has(tournament)) return "nations_league";
+  if (regionalChampionship.has(tournament)) return "regional_championship";
+  if (regionalQualifier.has(tournament)) return "regional_qualifier";
+  if (friendly.has(tournament)) return "friendly";
+
+  return "minor_other";
 };
+
 
 const normalizeRow = (row: RawMatchRow): MatchRow => ({
   date: new Date(row.date),
@@ -98,11 +255,156 @@ const writeStringRowsToCsv = async (rawMatches: RawMatchRow[], outputFilePath: s
   await fs.promises.writeFile(outputFilePath, csvRows, "utf-8");
 }
 
+const calulateGameHistory = (rowsSortedByDate: MatchRow[]): Map<string, GameOutcomeInfo[]> => {
+
+  const teamToRowIndex = new Map<string, GameOutcomeInfo[]>();
+
+  for (let i = 0; i < rowsSortedByDate.length; i++) {
+    const currMatch = rowsSortedByDate[i];
+
+    if (currMatch) {
+      const currTeamA = currMatch.homeTeam;
+      const currTeamB = currMatch.awayTeam;
+
+      const currTeamAGameOutcome = teamToRowIndex.get(currTeamA) ?? [];
+      const currTeamBGameOutcome = teamToRowIndex.get(currTeamB) ?? [];
+      let outcomeA;
+      let outcomeB;
+
+      if (currMatch.homeScore > currMatch.awayScore) {
+        outcomeA = 'win';
+        outcomeB = 'lose'
+      } else if (currMatch.homeScore < currMatch.awayScore) {
+        outcomeA = 'lose';
+        outcomeB = 'win'
+      } else {
+        outcomeA = outcomeB = 'draw'
+      }
+
+      const newTeamAGameOutcome = { rowIndex: i, goalFor: currMatch.homeScore, goalAgainst: currMatch.awayScore, outcome: outcomeA };
+      const newTeamBGameOutcome = { rowIndex: i, goalFor: currMatch.awayScore, goalAgainst: currMatch.homeScore, outcome: outcomeB };
+
+      teamToRowIndex.set(currTeamA, [...currTeamAGameOutcome, newTeamAGameOutcome]);
+      teamToRowIndex.set(currTeamB, [...currTeamBGameOutcome, newTeamBGameOutcome]);
+    }
+  }
+
+  return teamToRowIndex;
+}
+
+
+const calculateDifferences = (teamHistory: Map<string, GameOutcomeInfo[]>, matches: MatchRow[]): MatchRowWithGoals[] => {
+
+  const rowsWithGoals: MatchRowWithGoals[] = [];
+  for (let i = 0; i < matches.length; i++) {
+    const row = matches[i];
+
+    if (row !== undefined) {
+      const homeTeamHistory = teamHistory.get(row.homeTeam);
+      const awayTeamHistory = teamHistory.get(row.awayTeam);
+
+
+      let homeTeamPerformanceSlice: GameOutcomeInfo[];
+      let awayTeamPerformanceSlice: GameOutcomeInfo[];
+
+
+      if (homeTeamHistory && awayTeamHistory) {
+
+        const homeIndex = homeTeamHistory.findIndex((history) => history.rowIndex === i);
+        const awayIndex = awayTeamHistory.findIndex((history) => history.rowIndex === i);
+
+        if (homeIndex >= 5 && awayIndex >= 5) {
+          homeTeamPerformanceSlice = homeTeamHistory.slice(homeIndex - 5, homeIndex)
+          awayTeamPerformanceSlice = awayTeamHistory.slice(homeIndex - 5, homeIndex)
+
+          if (homeTeamPerformanceSlice && awayTeamPerformanceSlice) {
+            rowsWithGoals.push(computePerformanceForRow(row, homeTeamPerformanceSlice, awayTeamPerformanceSlice));
+          }
+        } else {
+
+          continue;
+        }
+      }
+    }
+
+  }
+  return rowsWithGoals;
+}
+
+
+const computePerformanceForRow = (row: MatchRow, homeTeamPerformanceSlice: GameOutcomeInfo[], awayTeamPerformanceSlice: GameOutcomeInfo[]): MatchRowWithGoals => {
+
+  const formScore = (outcome: string): number => {
+    if (outcome === "win") return 1;
+    if (outcome === "draw") return 0.5;
+    return 0;
+  };
+
+  let totalGoalsScoredHome = 0;
+  let totalGoalsConceededHome = 0;
+  let totalFormHome = 0;
+
+  let totalGoalsScoredAway = 0;
+  let totalGoalsConceededAway = 0;
+  let totalFormAway = 0;
+
+  homeTeamPerformanceSlice.forEach((history) => {
+    totalGoalsScoredHome += history.goalFor;
+    totalGoalsConceededHome += history.goalAgainst;
+    totalFormHome += formScore(history.outcome);
+  })
+
+  awayTeamPerformanceSlice.forEach((history) => {
+    totalGoalsScoredAway += history.goalFor;
+    totalGoalsConceededAway += history.goalAgainst;
+    totalFormAway += formScore(history.outcome);
+  })
+
+  const avgGoalsScoredHome = totalGoalsScoredHome / 5;
+  const avgGoalsConceededHome = totalGoalsConceededHome / 5;
+
+  const avgGoalsScoredAway = totalGoalsScoredAway / 5;
+  const avgGoalsConceededAway = totalGoalsConceededAway / 5;
+
+  const rowWithGoals: MatchRowWithGoals = {
+    ...row,
+    home_form_last5: totalFormHome,
+    home_avg_goals_scored: avgGoalsScoredHome,
+    home_avg_goals_conceded: avgGoalsConceededHome,
+    away_form_last5: totalFormAway,
+    away_avg_goals_scored: avgGoalsScoredAway,
+    away_avg_goals_conceded: avgGoalsConceededAway,
+    diff_form_last5: totalFormHome - totalFormAway,
+    diff_avg_goals_scored: avgGoalsScoredHome - avgGoalsScoredAway,
+    diff_avg_goals_conceded: avgGoalsConceededHome - avgGoalsConceededAway,
+  }
+
+  return rowWithGoals;
+}
+
+const calculateElo = (rowsWithGoals: MatchRowWithGoals[]) => {
+
+  const updatedEloMap: Record<string, number> = {};
+
+  rowsWithGoals.forEach((row) => {
+
+    const homeTeam = row.homeTeam;
+    const awayTeam = row.awayTeam;
+
+    const expectedResultHome = 1/ (1 + Math.pow(10, ))
+  });
+}
+
 export const cleanData = async (inputFilePath: string, trainingOutputFilePath: string, testingOutputFilePath: string): Promise<boolean> => {
   const preProcessedMatchRows: MatchRow[] = await processCsv(inputFilePath);
   if (preProcessedMatchRows.length === 0) {
     throw new Error('No rows to read');
   }
+
+  const teamGameHistory = calulateGameHistory(preProcessedMatchRows);
+  const teamRowsWithDifferences = calculateDifferences(teamGameHistory, preProcessedMatchRows);
+
+  const teamRowsWithGoalsAndElo = calculateElo(teamRowsWithDifferences);
 
   const trainingSet: MatchRow[] = filterMatches(preProcessedMatchRows, new Date('2006-01-01'), new Date('2019-01-01'));
   const testingSet: MatchRow[] = filterMatches(preProcessedMatchRows, new Date('2020-01-01'), new Date('2026-01-01'));
--- src/types/match.ts
+++ src/types/match.ts
@@ -1,4 +1,4 @@
-export interface RawMatchRow {
+export type RawMatchRow = {
   date: string;
   home_team: string;
   away_team: string;
@@ -9,7 +9,7 @@ export interface RawMatchRow {
   neutral: string;
 }
 
-export interface MatchRow {
+export type MatchRow = {
   date: Date;
   homeTeam: string;
   awayTeam: string;
@@ -30,4 +30,34 @@ export const headers: (keyof RawMatchRow)[] = [
   "country",
   "neutral",
 ];
+export type MatchRowWithGoals = MatchRow & {
+  home_form_last5: number;
+  home_avg_goals_scored: number;
+  home_avg_goals_conceded: number;
+  away_form_last5: number;
+  away_avg_goals_scored: number;
+  away_avg_goals_conceded: number;
+  diff_form_last5: number;
+  diff_avg_goals_scored: number;
+  diff_avg_goals_conceded: number;
+};
+
+export type GameOutcomeInfo = {
+  rowIndex: number,
+  goalFor: number,
+  goalAgainst: number,
+  outcome: string,
+};
+
+export const ELO_K_MAP = {
+  world_cup: 60,
+  world_cup_qualifier: 50,
+  continental_championship: 45,
+  continental_qualifier: 35,
+  nations_league: 30,
+  regional_championship: 25,
+  regional_qualifier: 20,
+  friendly: 15,
+  minor_other: 10,
+};
 
